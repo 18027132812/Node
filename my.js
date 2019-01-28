e12635c85077603ef9f9140223b04e55795b8ae4
v4 <lucky6616888@163.com>
2019-01-28 15:57:01 +0800

    no message

--- my.js
+++ my.js
@@ -24,7 +24,7 @@ const conn = mysql.createConnection({
 });
 
 //  get表中所有数据
-app.get('/api/getlist', (req, res) => {
+app.get('/api/getUser', (req, res) => {
   const sqlStr = 'select * from user '
   conn.query(sqlStr, (err, results) => {
     if (err) return res.json({ err_code: 1, message: '资料不存在', affextedRows: 0 })
@@ -33,7 +33,7 @@ app.get('/api/getlist', (req, res) => {
 });
 
 //  按条件查询
-app.get('/api/getlistdetl', (req, res) => {
+app.get('/api/getUserDetail', (req, res) => {
   const number = req.query.number
   const sqlStr = 'select * from user where phone=?'
   conn.query(sqlStr, number, (err, results) => {
@@ -43,16 +43,22 @@ app.get('/api/getlistdetl', (req, res) => {
 });
 
 //添加
-app.post('/api/addcard', (req, res) => {
-  const user = req.body
-  user.ctime = moment().format('YYYY-MM-DD HH:mm:ss')
+app.post('/api/addUser', (req, res) => {
+  const user = {
+    phone: '18027132812',
+    password: '123456',
+    nick_name: 'v4',
+    up_time: new Date().getTime(),
+    login_time: new Date().getTime(),
+    register_type: 1
+  };
   const sqlStr = 'insert into user set ?'
   conn.query(sqlStr, user, (err, results) => {
-    if (err) return res.json({ err_code: 1, message: err, affectedRows: 0 })
-    res.json({ err_code: 200, message: '恭喜添加数据成功', affectedRows: results.affectedRows })
+    if (err) return res.json({ err_code: 1, message: err, affectedRows: 0 });
+    res.json({ err_code: 200, message: '添加数据成功', affectedRows: results.affectedRows });
   })
 });
 
-app.listen(3000, () => {
-  console.log('正在监听端口3000,http://127.0.0.1:3000');
+app.listen(3030, () => {
+  console.log('正在监听端口3000,http://127.0.0.1:3030');
 });
